550115c31493fad54fa7069e41f9a9d2305ac415
kjkumarswamy <kumarhpt55@gmail.com>
2021-12-12 18:41:13 +0530

    updated index
    update

--- src/index.server.js
+++ src/index.server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const connectDb = require("./config/db");
 const cors = require("cors");
+const path = require("path");
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
@@ -21,9 +22,16 @@ app.use("/api", userRoutes);
 app.use("/api", postRoutes);
 app.use("/api", categoryRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hi this si working");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Hi this si working");
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
