b5731d5eb3070e996d728ec468495dd9ea4bf28b
kjkumarswamy <kumarhpt55@gmail.com>
2021-12-13 13:44:46 +0530

    updated post
    sada

--- client/src/redux/action/post.Action.js
+++ client/src/redux/action/post.Action.js
@@ -18,7 +18,7 @@ export const createPostAction = (form) => {
         });
         dispatch(getAllPostsAction());
         const { post } = res.data;
-        history.go(`/single/${post._id}`);
+        history.location(`/single/${post._id}`);
       } else {
         if (res.status === 400) {
           dispatch({
